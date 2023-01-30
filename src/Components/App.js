9a5626a7ff7091cda66f1c5e54dc2af7e966b49f
GWNR71517\joshu <joshuabliven93@gmail.com>
2023-01-29 20:12:44 -0500

    created mock data file, search bar returns data and filters for unfound entries

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,15 @@
-import {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import '../index.css'
+import {weatherData} from "../data.js"
 
 function Submit(props) {
   const onClick = () => {
     const search = document.getElementById('search')
     props.setResult(search.value)
-    search.value = ''
+    props.setIsSearching(true)
+    if (search.value == '') {
+      props.setIsSearching(false)
+    }
   }
   const handleKeyDown = (event) => {
     if (event.key === 'Enter') {
@@ -14,6 +18,9 @@ function Submit(props) {
   }
   return (
     <div className="banner">
+      <h1 className="tittie">
+        Spicy Weather
+      </h1>
       <div className="searchBar">
         <input
           id="search"
@@ -30,19 +37,44 @@ function Submit(props) {
 }
 
 function Result(props) {
-  return (
-    <div>
-      {props.value}
-    </div>
+  const filterResults = weatherData.filter((data) => {
+      return props.value.toLowerCase() == data.name
+    }
   )
+  if (props.isSearching == true)
+    if (!filterResults || filterResults.length === 0) {
+      filterResults.push({
+        name: `Result ${props.value} Not Found`,
+        temp: "",
+        spice: ""
+      })
+    }
+    return (
+      <div>
+        <div className="resultContainer">
+          {filterResults.map((data, key) => {
+            return (
+              <div key={key}>
+                {data.name +
+                ", " +
+                data.temp +
+                ", " +
+                data.spice}
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    )
 }
 
 function App() {
+  const [isSearching, setIsSearching] = useState(false)
   const [result, setResult] = useState('')
   return (
     <div>
-      <Submit setResult={setResult} />
-      <Result value={result} />
+      <Submit setResult={setResult} setIsSearching={setIsSearching} />
+      <Result value={result} isSearching={isSearching}/>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const weatherData = [
+	{
+		"id": 1,
+		"name": "dallas",
+		"temp": "75 deg",
+		"spice": "Schweaty Balls"
+	},
+	{
+		"id": 2,
+		"name": "chicago",
+		"temp": "24 deg",
+		"spice": "My nipples could cut glass"
+	},
+	{
+		"id": 3,
+		"name": "philadelphia",
+		"temp": "37 deg",
+		"spice": "wear a jacket, douche-canoe"
+	}
+]
